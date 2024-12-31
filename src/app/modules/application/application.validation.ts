5ed385cf9364d792be65eea67edacd29783f3a9f
tanvirch0750 <tanvirch7575@gmail.com>
2024-12-31 12:32:43 +0600

    feat: update application model and validation to make interview details optional and improve schema readability

--- src/app/modules/application/application.validation.ts
+++ src/app/modules/application/application.validation.ts
@@ -3,18 +3,19 @@ import { z } from 'zod';
 const interviewDetailsSchema = z.object({
   date: z
     .string()
-    .refine((val) => !isNaN(Date.parse(val)), 'Invalid date format'),
+    .refine((val) => !isNaN(Date.parse(val)), 'Invalid date format')
+    .optional(),
   time: z
     .string()
-    .regex(/^\d{2}:\d{2}$/, 'Invalid time format (HH:MM expected)'),
-  location: z.string().min(1, 'Location is required'),
+    .regex(/^\d{2}:\d{2}$/, 'Invalid time format (HH:MM expected)')
+    .optional(),
+  location: z.string().min(1, 'Location is required').optional(),
 });
 
 const createApplicationSchema = z.object({
   body: z.object({
     applicationGroupId: z.string(),
     jobTitle: z.string().min(1, 'Job title is required'),
-    jobRole: z.string().min(1, 'Job role is required'),
     companyName: z.string().min(1, 'Company name is required').optional(),
     companyEmail: z.string().email('Invalid email address').optional(),
     companyWebsite: z.string().min(1, 'Phone number is required').optional(),
@@ -25,7 +26,17 @@ const createApplicationSchema = z.object({
     jobPortal: z.string().min(1, 'Job portal is required').optional(),
     address: z.string().min(1, 'Address is required').optional(),
     jobType: z.enum(['remote', 'onsite', 'hybrid']),
-    status: z.enum(['Applied', 'Interview_Scheduled', 'Rejected', 'Under_Review', 'Task_Received', 'Task_Ongoing', 'Task_Submitted', 'Offer_Received', 'Offer_Accepted'],),
+    status: z.enum([
+      'Applied',
+      'Interview_Scheduled',
+      'Rejected',
+      'Under_Review',
+      'Task_Received',
+      'Task_Ongoing',
+      'Task_Submitted',
+      'Offer_Received',
+      'Offer_Accepted',
+    ]),
     appliedDate: z
       .string()
       .refine((val) => !isNaN(Date.parse(val)), 'Invalid date format')
